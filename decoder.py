25a066ed48352ba5514125de128e1cded38de306
Djara2 <62144343+Djara2@users.noreply.github.com>
2022-09-01 16:04:47 -0500

    Modularized the program so that the main file (squidencoder.py) is now 130 lines instead of 781 (651 lines leaner for the main program)

---
+++ decoder.py
@@ -0,0 +1,245 @@
+def decode(start, d):
+    start_og = start
+    start = start.split()
+    encode = ""
+
+    for word in start:
+        if word in d:
+            encode += d[word]
+            encode += " "
+        else:
+            x = 0
+            while x < len(word):
+                char = word[x]
+                        
+                if char == "Д":
+                    encode += "d"
+                
+                elif char in ["d", "z", "a", "i", "n", "j", "e"]:
+                    if (encode[len(encode)-3:] == "ing") or (encode[len(encode)-2:] == "ce") or (encode[len(encode)-2:] == "se") or (encode[len(encode)-2:] == "nt") or (encode[len(encode)-2:] == "ly") or (encode[len(encode)-2:] == "et"):
+                        if char != "e":
+                            encode += ""
+    
+                    else:
+                        encode += char
+                
+                # ss
+                # st
+                elif char == "ß":
+                    encode += "ss"
+                
+                elif char == "š":
+                    encode += "st"
+                
+                elif char == "ç":
+                    if x + 1 < len(word):
+                        if word[x+1] == "a":
+                            if x + 2 < len(word):
+                                if word[x+2] == "n":
+                                    if x + 3 < len(word):
+                                        if word[x+3] == "d":
+                                            encode += "se"
+                                        else:
+                                            encode += char
+                                    else:
+                                        encode += char
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        else:
+                            encode += char
+                    else:
+                        encode += char
+
+                elif char == "k":
+                    if x + 1 < len(word):
+                        if word[x+1] == "a":
+                            if x + 2 < len(word):
+                                if word[x+2] == "n":
+                                    if x + 3 < len(word):
+                                        if word[x+3] == "d":
+                                            encode += "ce"
+                                        else:
+                                            encode += char
+                                    else:
+                                        encode += char
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        else:
+                            encode += char
+                    else:
+                        encode += char
+                    
+                elif char == "ë":
+                    encode += "ea"
+                
+                elif char == "ä":
+                    encode += "ee"
+                
+                elif char == "δ":
+                    encode += "ge"
+                
+                elif char == "μ":
+                    encode += "re"
+                
+                elif char == "ю":
+                    encode += "ity"
+                
+                elif char == "ц":
+                    encode += "ium"
+                
+                elif char == "ü":
+                    encode += "ion"
+                
+                elif char == "ö":
+                    encode += "ry"
+                
+                elif char == "ø":
+                    encode += "ua"
+                
+                elif char == "λ":
+                    encode += "me"
+                
+                elif char == "ඞ":
+                    encode += "sus"
+                
+                elif char == " й":
+                    encode += "mn"
+                
+                elif char == "б":
+                    encode += "ie"
+                
+                elif char == "-":
+                    if x + 1 < len(word):
+                        # -ll → er
+                        if word[x+1] == "l":
+                            if x + 2  < len(word):
+                                if word[x+2] == "l":
+                                    encode += "er"
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        
+                        # -il  → or and -iij → ly
+                        elif word[x+1] == "i":
+                            if x + 2 < len(word):
+                                #-il  → or
+                                if word[x+2] == "l":
+                                    encode += "or"
+                                #-iij → ly
+                                elif word[x+2] == "i":
+                                    if x + 3 < len(word):
+                                        if word[x+3] == "j":
+                                            encode += "ly"
+                                        else:
+                                            encode += char
+                                    else:
+                                        encode += char
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        
+                        # -ejd → et
+                        elif word[x+1] == "e":
+                            if x + 2 < len(word):
+                                if word[x+2] == "j":
+                                    if x + 3 < len(word):
+                                        if word[x+3] == "d":
+                                            encode += "et"
+                                        else:
+                                            encode += char
+                                    else:
+                                        encode += char
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        
+                        #-kyo → ology
+                        elif word[x+1] == "k":
+                            if x + 2 < len(word):
+                                if word[x+2] == "y":
+                                    if x + 3 < len(word):
+                                        if word[x+3] == "o":
+                                            encode += "ology"
+                                        else:
+                                            encode += char
+                                    else:
+                                        encode += char
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                    
+                        #-dzai → ing
+                        elif word[x+1] == "d":
+                            if x + 2 < len(word):
+                                if word[x+2] == "z":
+                                    if x + 3 < len(word):
+                                        if word[x+3] == "a":
+                                            if x + 4 < len(word):
+                                                if word[x+4] == "i":
+                                                    encode += "ing"
+                                                else:
+                                                    encode += char
+                                            else:
+                                                encode += char
+                                        else:
+                                            encode += char
+                                    else:
+                                        encode += char
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        else:
+                            encode += char
+                    else:
+                        encode += char
+                
+                elif char == "l":
+                    if encode[len(encode)-1] != "r" and encode[len(encode)-2] != "e":
+                        if x + 1 < len(word):
+                            if word[x+1] == "n":
+                                if x + 2 < len(word):
+                                    if word[x+2] == "j":
+                                        encode += "nt"
+                                    else:
+                                        encode += char
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        else:
+                            encode += char
+                
+                elif char == "\'":
+                    if x + 1 < len(word):
+                        encode += f"{word[x+1]}"
+                else:
+                    """
+                    if x + 1 < len(word):
+                        if word[x+1] == char:
+                            encode += f"\'{char}"
+                            x += 1
+                        else:
+                            encode += char
+                    else:
+                        encode += word[x]
+                    """
+
+                    encode += char
+                x += 1
+            encode += " "
+    print(f"Input: {start_og}")
+    print(f"\n\nDecode result: {encode}")
+    try:
+        pyclip.copy(encode)
+        print("\n\nThe decoded result has been copied to your clipboard")
+    except:
+        print("\nThe encoded result could not be copied to your clipboard because the pyclip module does not work with your platform.")
---
+++ encoder.py
@@ -0,0 +1,252 @@
+def encode(start, reserved_words_dictionary):
+    start = start.split()
+    encode = ""
+    for word in start:
+        if word in reserved_words_dictionary:
+            encode += reserved_words_dictionary[word]
+            encode += " "
+        else:
+            x = 0
+            while x < len(word):
+                char = word[x]
+                
+
+                if char == "d":
+                    if x + 1 < len(word):
+                        if word[x+1] == char:
+                            encode += "\'Д"
+                            x += 1
+                        else:
+                            encode += "Д"
+                    else:
+                        encode += "Д"
+
+                # ss
+                # st
+                elif char == "s":
+                    if x + 1 < len(word):
+                        if word[x+1] == "s":
+                            encode += "ß"
+                            x += 1
+                        elif word[x+1] == "t":
+                            encode += "š"
+                            x += 1
+                        elif word[x+1] == "e":
+                            if x + 2 < len(word):
+                                if word[x+2] == "e" or word[x+2] == "a":
+                                    encode += char
+                                else:
+                                    encode += "çand"
+                                    x += 1
+                            else:
+                                encode += "çand"
+                                x += 1
+                        else:
+                            encode += char
+                    else:
+                        encode += char
+                elif char == "c":
+                    if x + 1 < len(word):
+                        if word[x+1] == "e":
+                            encode += "kand" 
+                            x += 1
+                        else:
+                            encode += char
+                    else:
+                        encode += char
+                # ea
+                # ee
+                # er
+                # et
+                elif char == "e":
+                    if x + 1 < len(word):
+                        if word[x+1] == "a":
+                            encode += "ë"
+                            x += 1
+                        elif word[x+1] == "e":
+                            encode += "ä"
+                            x += 1
+                        elif word[x+1] == "r":
+                            encode += "-ll"
+                            x += 1
+                        elif word[x+1] == "t":
+                            encode += "-ejd"
+                            x += 1
+                        else:
+                            encode += char
+                    else:
+                        encode += char
+                elif char == "u":
+                    if x + 1 < len(word):
+                        if word[x+1] == "a":
+                            encode += "ø"
+                            x += 1
+                        else: 
+                            encode += char
+                    else:
+                        encode += char
+                # or
+                # ology
+                elif char == "o":
+                    if x + 1 < len(word):
+                        if word[x+1] == "r":
+                            encode += "-il"
+                            x += 1
+                        elif word[x+1] == "o":
+                            encode += "\'o"
+                            x += 1
+                        elif word[x:len(word)] == "logy":
+                            encode += "-kyo"
+                            x = len(word)
+                        else:
+                            encode += char
+                    else:
+                        encode += char
+
+                # ity
+                # ium
+                # ion
+                # ing
+                elif char == "i":
+                    if x + 1 < len(word):
+                        if word[x+1] == "e":
+                            encode += "б"
+                            x += 1
+                        
+                        # ing
+                        elif word[x+1] == "n":
+                            if x + 2 < len(word):
+                                if word[x+2] == "g":
+                                    encode += "-dzai"
+                                    x += 2
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        
+                        # ity
+                        elif word[x+1] == "t":
+                            if x + 2 < len(word):
+                                if word[x+2] == "y":
+                                    encode += "ю"
+                                    x += 2
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        # ium
+                        elif word[x+1] == "u":
+                            if x + 2 < len(word):
+                                if word[x+2] == "m":
+                                    encode += "ц"
+                                    x += 2
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        # ion
+                        elif word[x+1] == "o":
+                            if x + 2 < len(word):
+                                if word[x+2] == "n":
+                                    encode += "ü"
+                                    x += 2
+                                else:
+                                    encode += char
+                            else:
+                                encode += char
+                        else:
+                            encode += char
+                    else:
+                        encode += char
+
+                elif char == "g":
+                    if x + 1 < len(word):
+                        if word[x+1] == "e":
+                            encode += "δ"
+                            x += 1
+                        elif word[x+1] == "g":
+                            encode += "\'g"
+                            x += 1
+                        else:
+                            encode += "g"
+                    else:
+                        encode += "g"
+
+                elif char == "r":
+                    if x + 1 < len(word):
+                        if word[x+1] == "y":
+                            encode += "ö"
+                            x += 1
+                        elif word[x+1] == "e":
+                            encode += "μ"
+                            x += 1
+                        elif word[x+1] == "r":
+                            encode += "\'r"
+                            x += 1
+                        else:
+                            encode += char
+                    else:
+                        encode += "r"
+                
+                elif char == "m":
+                    if x + 1 < len(word):
+                        if word[x+1] == "e":
+                            encode += "λ"
+                            x += 1
+                        elif word[x+1] == "n":
+                            encode += "й"
+                            x += 1
+                        elif word[x+1] == "m":
+                            encode += "\'m"
+                            x += 1
+                        else:
+                            encode += "m"
+                    else:
+                        encode += char
+
+                elif char == "n":
+                    if x + 1 < len(word):
+                        if word[x+1] == "t":
+                            encode += "lnj"
+                            x += 1
+                        elif word[x+1] == "n":
+                            encode += "\'n"
+                            x += 1
+                        else:
+                            encode += char
+                    else:
+                        encode += char
+
+                elif char == "l":
+                    if x + 1 < len(word):
+                        if word[x+1] == "y":
+                            encode += "-iij"
+                            x += 1
+                        elif word[x+1] == "l":
+                            encode += "\'l"
+                            x += 1
+                        else:
+                            encode += char
+                    else:
+                        encode += char
+
+                else:
+                    if x + 1 < len(word):
+                        if word[x+1] == char:
+                            encode += f"\'{char}"
+                            x += 1
+                        else:
+                            if char != "\'":
+                                encode += char
+                    else:
+                        if char != "\'":
+                            encode += word[x]
+                x += 1
+            encode += " "
+
+    print(f"\nEncode result: {encode}")
+    try:
+        pyclip.copy(encode)
+        print("\nThe encoded result has been copied to your clipboard")
+    except:
+        print("\nThe encoded result could not be copied to your clipboard because the pyclip module does not work with your platform.")
